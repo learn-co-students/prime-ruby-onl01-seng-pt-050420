8b2290e979aaf89e7aa845d23cbf497e238002ab
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-08 01:06:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(int)
+  int.positive? ? multiples = (2...int).to_a : multiples = (int+1..1).to_a
+  multiples.delete(0)
+  
+  return false if int == 1
+  
+  !multiples.any? do |num|
+    int % num == 0 
+  end
+end
