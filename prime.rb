9958b68bbf743191af689db4742844ce271b8a3f
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-15 04:29:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(integer)
+  start = 2
+  if integer > 1
+    range = (start..integer-1).to_a
+    range.none? do |num|
+      integer % num == 0
+    end
+  else FALSE
+  end
+  end
